683c945b69546844e8a9a972ee3e505fc8906d36
Adi Kusuma <144588474+adikusum4@users.noreply.github.com>
2024-10-26 10:33:26 +0000

    update question 3 and 4

--- 05-deployment/homework/question-3.py
+++ 05-deployment/homework/question-3.py
@@ -1,7 +1,18 @@
-import requests
+import pickle
 
-url = "http://127.0.0.1:9696/predict"
-client = {"job": "student", "duration": 280, "poutcome": "failure"}
+# Muat DictVectorizer dan LogisticRegression model
+with open("dv.bin", "rb") as f_in:
+    dv = pickle.load(f_in)
 
-response = requests.post(url, json=client).json()
-print("Probability of subscription:", response["probability"])
+with open("model1.bin", "rb") as f_in:
+    model = pickle.load(f_in)
+
+# Data klien
+client = {"job": "management", "duration": 400, "poutcome": "success"}
+
+# Transformasi data klien menggunakan DictVectorizer
+X = dv.transform([client])
+
+# Dapatkan probabilitas prediksi
+probability = model.predict_proba(X)[0, 1]
+print(f"Probability of subscription: {probability}")
