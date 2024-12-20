ccccd3ea97c9366677bdaa1d5f97f883de1d8ac0
Adi Kusuma <144588474+adikusum4@users.noreply.github.com>
2024-12-20 08:08:43 +0000

    update

--- 10-kubernetes/homework/q6_test.py
+++ 10-kubernetes/homework/q6_test.py
@@ -1,3 +1,4 @@
+import time
 import requests
 
 
@@ -6,4 +7,9 @@ url = "http://localhost:9696/predict"
 client = {"job": "management", "duration": 400, "poutcome": "success"}
 response = requests.post(url, json=client).json()
 
-print(response)
+while True:
+    time.sleep(0.1) #sleep(0.1)  # Adjust the frequency if necessary
+    response = requests.post(url, json=client).json()
+    print(response)
+
+#print(response)
